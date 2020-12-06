a3bd8647e76cb00505a731cee663f4896eee3f92
sreechand <sreechand2002@gmail.com>
2020-12-06 23:41:32 +0530

    added be color for header

--- client/src/HomePage/Header.jsx
+++ client/src/HomePage/Header.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Button, Grid, Image } from 'semantic-ui-react'
 const Headers = () => {
     return (
-        <div style={{ padding: '1rem', border: '1px solid black' }}>
+        <div style={{ border: '1px solid black', backgroundColor: 'lightblue' }}>
             <Grid>
                 <Grid.Row>
                     <Grid.Column width={2}>
                         <Image src='images/icon.jpg' style={{ width: '120px', margin: '0 2rem' }} />
                     </Grid.Column>
                     <Grid.Column style={{ marginRight: '2rem' }} width={13} verticalAlign='middle' textAlign='right'>
-                        <Button basic>Login</Button>
-                        <Button basic>SignUp</Button>
+                        <Button color='yellow' basic>Login</Button>
+                        <Button color='blue' basic>SignUp</Button>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
