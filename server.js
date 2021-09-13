889a2edb4d5da2c2dc573bd1848e11d038f1b656
Adi7171 <adim71712@gmail.com>
2021-09-13 17:16:44 +0530

    Successfully added Backend by using node and express ,added server.js ,changed and created package.json and package-lock.json files, modified App.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`)); //
+
+app.get("/express_backend", (req, res) => {
+  res.send({ express: "Helloworld" });
+});
