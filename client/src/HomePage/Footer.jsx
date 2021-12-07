c919259f5fcc4cc1727f25c624844c6958ef6041
tharunbirla <stub.tharun5573@kvschennairegion.in>
2021-12-07 17:29:25 +0530

    Added footer

---
+++ client/src/HomePage/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <div className="ui inverted vertical footer segment">
+                <div className="ui center aligned container">
+                    <h4 className="ui inverted header">&copy; Copyright 2021 | All rights reserved</h4>
+                    <a href="https://github.com/adityabisoi/startup-incubator" target="_blank"><i className="github square icon big"></i></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
