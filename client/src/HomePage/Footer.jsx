3fc0c1a44d162e7d0d13c4eaff1a26aa769f455e
Aditya Bisoi <adityabisoi1999@gmail.com>
2021-12-08 08:44:00 +0530

    add footer

--- client/src/HomePage/Footer.jsx
+++ client/src/HomePage/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="Footer">
             <div className="ui inverted vertical footer segment">
                 <div className="ui center aligned container">
-                    <h4 className="ui inverted header">&copy; Copyright 2021 | All rights reserved</h4>
+                    <h4 className="ui inverted header">&copy; 2021 Aditya Bisoi</h4>
                     <a href="https://github.com/adityabisoi/startup-incubator" target="_blank"><i className="github square icon big"></i></a>
                 </div>
             </div>
