e51dd0c76f2c45042ae4a7e5c880db15a58219f7
Tejas <tejasftw117@gmail.com>
2022-03-04 09:24:16 +0530

    Added login and singup components

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Headers from "./HomePage/Header";
 import Body from "./HomePage/Body";
 import Footer from "./HomePage/Footer";
 import Product from './Components/Product/Product'
+import Login from "./Login/Login";
+import Register from "./Register/Register";
 import ScrollButton from './Components/ScrollButton/ScrollButton';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -26,9 +28,11 @@ function App() {
             <Route path="product_details">
               <Route path=":product_id" element={<Product />}></Route>
             </Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<Register />}></Route>
           </Routes>
 
-          {/* <p>{!data ? "Loading..." : data}</p> */}
+           {/* <p>{!data ? "Loading..." : data}</p> */}
           {/* The above line is replaced by helloworld when backend is running */}
           <ScrollButton />
           <Footer />
--- client/src/HomePage/Header.jsx
+++ client/src/HomePage/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Button, Grid, Image } from 'semantic-ui-react'
+import { Button, Grid, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
 const Headers = () => {
     return (
         <div style={{ border: '1px solid black', backgroundColor: 'lightblue' }}>
@@ -9,8 +11,14 @@ const Headers = () => {
                         <Image src='Images/icon.jpg' style={{ width: '120px', margin: '0 2rem' }} />
                     </Grid.Column>
                     <Grid.Column style={{ marginRight: '2rem' }} width={13} verticalAlign='middle' textAlign='right'>
-                        <Button color='yellow' basic>Login</Button>
-                        <Button color='blue' basic>SignUp</Button>
+
+                        <Link to='/login'>
+                            <Button color='yellow' basic >Login</Button>
+                        </Link>
+                        
+                        <Link to='/signup'>
+                            <Button color='blue' basic>SignUp</Button>
+                        </Link>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
