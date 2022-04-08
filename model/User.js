57926c1ee6056e8924a9e580d1a04a2e4dc85365
Ayush <ayushthth@gmail.com>
2022-04-08 13:14:44 +0530

    esLintAdded

--- model/User.js
+++ model/User.js
@@ -1,38 +1,39 @@
-const mongoose=require('mongoose')
-const validator = require('validator')
+/* eslint-disable linebreak-style */
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 
-    // This also validates whether the user has entered valid and unique email
-    email: {
-        type: String,
-        unique: true, 
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
+  // This also validates whether the user has entered valid and unique email
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
     },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"')
-            }
-        }
-    }
-})
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "password"');
+      }
+    },
+  },
+});
 
-const User=mongoose.model('User', userSchema)
-module.exports=User
+const User = mongoose.model('User', userSchema);
+module.exports = User;
