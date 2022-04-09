21961f9050b13ccfb2aa91e674e7a049cb08cde9
Ayush <ayushthth@gmail.com>
2022-04-09 11:20:29 +0530

    Login/SignUp endpoints Created Db set

---
+++ db/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/startup-incubator',{})
+
+const connectToDatabase=()=>{console.log("Connected")}
+
+module.exports=connectToDatabase
+
+
+
+
