de77fcb8a24740199a669dea0047d715519eb32e
Siddhesh Jain <jainsiddhesh777@gmail.com>
2022-04-10 12:52:36 +0530

    Products displayed in the form of cards

--- client/src/Components/Product/ProductPreview.jsx
+++ client/src/Components/Product/ProductPreview.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Grid, Button, Icon, Image, Header } from "semantic-ui-react";
-import {dummyProducts} from "../../utils/constants"
+import { dummyProducts } from "../../utils/constants";
 
 const ProductPreview = ({ id, heading, description, image, likes }) => {
   let linkStyle = {
@@ -11,35 +11,55 @@ const ProductPreview = ({ id, heading, description, image, likes }) => {
   };
 
   const [currentLikes, setCurrentLikes] = React.useState(likes);
-  const [str,setStr] = React.useState(JSON.stringify(dummyProducts));
+  const [str, setStr] = React.useState(JSON.stringify(dummyProducts));
 
   function changeLikes(e) {
-    setCurrentLikes(1+currentLikes);
+    setCurrentLikes(1 + currentLikes);
     const parsedObj = JSON.parse(str);
-    parsedObj[id - 1].likes = currentLikes+2;
-    console.log(parsedObj[id-1].likes)
+    parsedObj[id - 1].likes = currentLikes + 2;
+    console.log(parsedObj[id - 1].likes);
     const jsonObj = JSON.stringify(parsedObj);
     setStr(jsonObj);
-    localStorage.setItem("dummyProducts",str);
+    localStorage.setItem("dummyProducts", str);
   }
 
   return (
-    <Grid.Row style={{margin: "1rem", maxWidth: "400px", backgroundColor: "white", boxShadow: "2px 4px 4px 2px grey"}}>
-      <Grid.Row style={{ margin: "20px"}}>
+    <Grid.Row
+      style={{
+        margin: "1rem",
+        maxWidth: "400px",
+        backgroundColor: "white",
+        boxShadow: "2px 4px 4px 2px grey",
+      }}
+    >
+      <Grid.Row style={{ margin: "20px" }}>
         <Image src={image} size="large" />
       </Grid.Row>
-      <Grid.Row width={1} textAlign="left" style={{margin: "30px", marginTop:"0px",marginBottom:"0px"}}>
+      <Grid.Row
+        width={1}
+        textAlign="left"
+        style={{ margin: "30px", marginTop: "0px", marginBottom: "0px" }}
+      >
         <Header as="h1">{heading}</Header>
-        <p style={{fontSize: "16px"}}>{description}</p>
+        <p style={{ fontSize: "16px" }}>{description}</p>
       </Grid.Row>
-      <Grid.Row width={3} textAlign="right" verticalAlign="middle" style={{margin: "30px"}}>
-      <Link to={`/product_details/${id}`} class="link-1" style={{marginLeft:"10px", marginRight:"11rem"}}>
+      <Grid.Row
+        width={3}
+        textAlign="right"
+        verticalAlign="middle"
+        style={{ margin: "30px" }}
+      >
+        <Link
+          to={`/product_details/${id}`}
+          class="link-1"
+          style={{ marginLeft: "10px", marginRight: "11rem" }}
+        >
           Details
         </Link>
         <Button
           icon
           labelPosition="left"
-          style={{ height: "40px"}}
+          style={{ height: "40px" }}
           onClick={changeLikes}
         >
           <Icon name="arrow alternate circle up" />
--- client/src/HomePage/Header.jsx
+++ client/src/HomePage/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Headers = () => {
   return (
-    <div style={{backgroundColor: "lightblue" }}>
+    <div style={{ backgroundColor: "lightblue" }}>
       <Grid>
         <Grid.Row>
           <Grid.Column width={2}>
@@ -21,7 +21,7 @@ const Headers = () => {
             textAlign="right"
           >
             <Link to="/new-product">
-              <span style={{ color: "black", margin: "0 10px"}}>
+              <span style={{ color: "black", margin: "0 10px" }}>
                 New product
               </span>
             </Link>
