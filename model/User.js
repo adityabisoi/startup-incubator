4a8bf91dd079cf26109f05a29ad9e5c29b8d0557
Ayush Kangane <78008646+ayush25102001@users.noreply.github.com>
2022-04-12 08:29:34 +0530

    Update User.js

--- model/User.js
+++ model/User.js
@@ -1,6 +1,8 @@
 /* eslint-disable linebreak-style */
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -34,6 +36,28 @@ const userSchema = new mongoose.Schema({
     },
   },
 });
+
+// eslint-disable-next-line func-names
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  // eslint-disable-next-line no-use-before-define
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error('unable to login');
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('unable to login');
+  }
+  return user;
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
