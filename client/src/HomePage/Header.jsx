8985a02983d631d6e833ffe7c91b0db33e12c6a5
Siddhesh Jain <jainsiddhesh777@gmail.com>
2022-04-20 23:30:30 +0530

    new product button working

--- client/src/HomePage/Header.jsx
+++ client/src/HomePage/Header.jsx
@@ -11,7 +11,7 @@ const Headers = () => {
                         <img src="Images/icon.jpg" alt="" width="150" height="150" className="d-inline-block align-text-top" />
                     </a>
                     <div>
-                        <button className="btn bg-success butn" type="submit" style={{marginRight:'10px'}}>New Product</button>
+                        <a href='/new-product'><button className="btn bg-success butn" type="submit" style={{marginRight:'10px'}}>New Product</button></a>
                         <a href='/login'><button className="btn bg-warning butn" type="submit" style={{marginRight:'10px'}}>Login</button></a>
                         <a href='/signUp'><button className="btn bg-info butn" type="submit">SignUp</button></a>
                     </div>
