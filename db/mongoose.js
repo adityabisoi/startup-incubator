16e9a69b9140bf349817b5a35760d75fdd845cf2
Ayush <ayushthth@gmail.com>
2022-04-21 18:17:45 +0530

    ConnectionMade

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,19 +4,12 @@
 // const mongoose = require('mongoose');
 require('dotenv').config();
 
-// const DB = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.lgyhc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true`;
-// mongoose.connect(DB, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
+const DB = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.lgyhc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true`;
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
-//   .then(() => console.log('DATABASE CONNECTED'))
-//   .catch((err) => console.log(err));
+  .then(() => console.log('DATABASE CONNECTED'))
+  .catch((err) => console.log(err));
 
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/Start', {});
-
-const connectToDatabase = () => { console.log('Connected'); };
-
-module.exports = connectToDatabase;
