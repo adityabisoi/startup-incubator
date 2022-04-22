1d2f0f6d198995f73a5e5cf805abb50628cebc9b
Siddhesh Jain <jainsiddhesh777@gmail.com>
2022-04-22 13:20:26 +0530

    Home button added

--- client/src/HomePage/Header.jsx
+++ client/src/HomePage/Header.jsx
@@ -11,9 +11,10 @@ const Headers = () => {
                         <img src="Images/icon.jpg" alt="" width="150" height="150" className="d-inline-block align-text-top" />
                     </a>
                     <div>
-                        <a href='/new-product'><button className="btn bg-success butn" type="submit" style={{marginRight:'10px'}}>New Product</button></a>
-                        <a href='/login'><button className="btn bg-warning butn" type="submit" style={{marginRight:'10px'}}>Login</button></a>
-                        <a href='/signUp'><button className="btn bg-info butn" type="submit">SignUp</button></a>
+                        <a href='/'><button className="btn bg-danger butn" type="submit" style={{marginRight:'10px',color:"white"}}>Home</button></a>
+                        <a href='/new-product'><button className="btn bg-success butn" type="submit" style={{marginRight:'10px',color:"white"}}>New Product</button></a>
+                        <a href='/login'><button className="btn bg-warning butn" type="submit" style={{marginRight:'10px',color:"white"}}>Login</button></a>
+                        <a href='/signUp'><button className="btn bg-info butn" type="submit" style={{marginRight:'10px',color:"white"}}>SignUp</button></a>
                     </div>
                 </div>
             </nav>
