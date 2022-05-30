0ae1d966ca3a19a6f9ef031bce7d440ba0176124
Ayush <ayushthth@gmail.com>
2022-05-30 20:20:07 +0530

    PlaceHoldersForProfile

--- client/src/MyProfile/MyProfile.js
+++ client/src/MyProfile/MyProfile.js
@@ -1,14 +1,17 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import "./MyProfile.css";
-import { Link,useNavigate } from "react-router-dom";
-import {Button} from "semantic-ui-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "semantic-ui-react";
+// import { json } from "express/lib/response";
 
 function Register() {
   let history = useNavigate();
+  const [placeName,setplaceName]=useState("")
+  const [placeEmail,setplaceEmail]=useState("")
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const [contactNo,setContactNo] = useState("");
+  const [contactNo, setContactNo] = useState("");
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -16,85 +19,99 @@ function Register() {
       setMessage("Name shouldn't be empty");
     } else if (!email) {
       setMessage("Email shouldn't be empty");
-    } 
-    else
-    {
+    }
+    else {
 
-        //code here
+      //code here
     }
   }
   const handleClick = () => {
     localStorage.removeItem("token");
     history("/login");
   };
-  async function updateData(){
-      const res = await fetch('/updateData',{
-        method: 'POST',
-        headers:{
-          'content-type': 'application/json',
-          'auth-token':localStorage.getItem('token')
-        },
-        body: JSON.stringify({email:email,name:name})
-      })
-      await res.json();
-      setName(res.name);
-      setEmail(res.email);
+  useEffect(()=>{
+    fetch('http://localhost:5000/getCurrentUser', {
+      method:'GET',
+      headers:{
+        'Content-Type': 'application/json',
+        'auth-token': localStorage.getItem('token')
+      }
+    })
+    .then(response=>response.json())
+    .then(response=>{  
+      console.log(response)
+      setplaceEmail(response.email)
+      setplaceName(response.name)
+    });
+  },[]);
+  async function updateData() {
+    const res = await fetch('/updateData', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'auth-token': localStorage.getItem('token')
+      },
+      body: JSON.stringify({ email: email, name: name })
+    })
+    await res.json();
+    setName(res.name);
+    setEmail(res.email);
   }
 
-return (
+  return (
     <>
       <div className='profile-img'>
-        <img src="https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg" alt='profile' className="imgProfile"/>
+        <img src="https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg" alt='profile' className="imgProfile" />
       </div>
       <div className="data">
-      <div className='seperator'>
-        <hr />
-        <span>Display Name</span>
-      </div>
-      <form  onSubmit={updateData}>
-        <div className='form-control'>
-          <input
-            type='text'
-            name='name'
-            className='input'
-            placeholder='Name'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
+        <div className='seperator'>
+          <hr />
+          <span>Display Name</span>
         </div>
-        <div className='form-control'>
-          <input
-            type='email'
-            className='input'
-            placeholder='Email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+        <form onSubmit={updateData}>
+          <div className='form-control'>
+            <input
+              type='text'
+              name='name'
+              className='input'
+              placeholder={placeName}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className='form-control'>
+            <input
+              type='email'
+              className='input'
+              placeholder='Email'
+              value={placeEmail}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <Button color='green' type='submit' className='btn submit-btn'>
+            save
+          </Button>
+        </form>
+
+        <div className='seperator'>
+          <hr />
         </div>
-        <Button color='green' type='submit' className='btn submit-btn'>
-          save
-        </Button>
-      </form>
-      
-      <div className='seperator'>
-        <hr />
-      </div>
-      <div className='btn-container'>
-      <Button color='yellow' type='submit' className='btn submit-btn'>
-        <Link to='/MyProducts' style={{color:"white"}}>
-          Products
-        </Link>
-        </Button>
+        <div className='btn-container'>
+          <Button color='yellow' type='submit' className='btn submit-btn'>
+            <Link to='/MyProducts' style={{ color: "white" }}>
+              Products
+            </Link>
+          </Button>
         </div>
-        <div className='btn-container' style={{marginTop: '10px'}}>
-        <Button
-          color='red' type='submit' className='btn submit-btn'
-          onClick={handleClick}
-        >
-          Logout
-        </Button>
-        
-      </div>
+        <div className='btn-container' style={{ marginTop: '10px' }}>
+          <Button
+            color='red' type='submit' className='btn submit-btn'
+            onClick={handleClick}
+          >
+            Logout
+          </Button>
+
+        </div>
       </div>
     </>
   );
